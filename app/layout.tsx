77c5c50d34a1143311ce5bf436d80ea4d8788cc9
Ryosuke Futsuki <crayon155@gmail.com>
2025-11-22 17:01:59 +0900

    layout.tsx作成

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./globals.css";
+
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "To Do App",
+  description: "A simple to do app built with Next.js and Tailwind CSS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={`${notoSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
