f041fe8062143d70b9250cb8903121610c74f335
Ryosuke Futsuki <crayon155@gmail.com>
2025-11-30 21:54:33 +0900

    Suspense追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { TaskAddForm } from "./TaskAddForm";
 
 export default function Home() {
@@ -5,7 +6,9 @@ export default function Home() {
     <div className="flex min-h-screen justify-center bg-(--main-bg-color)">
       <main className="py-24 w-4/5 md:w-3/5 flex flex-col gap-7">
         <h1 className="text-5xl font-bold">Todo App</h1>
-        <TaskAddForm />
+        <Suspense>
+          <TaskAddForm />
+        </Suspense>
       </main>
     </div>
   );
